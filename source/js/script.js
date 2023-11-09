04e1cbd2c0609a6e3b15f65d8fafd7f25e971b88
Georgij Tsábiev <gtsabiev@inbox.ru>
2023-11-09 14:29:07 +0300

    Реализовывает появление модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -16,3 +16,33 @@ navToggle.addEventListener('click', function () {
     nav.classList.remove('nav--opened');
   }
 });
+
+/* Модальное окно */
+
+const modal = document.querySelector('.modal');
+const btn = document.querySelector('.knitted-baskets__order-button');
+const closeBtn = Array.from(document.querySelectorAll('.modal__add-button'));
+
+btn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.style.display = "flex";
+});
+
+closeBtn.forEach(closeBtn => {
+  closeBtn.addEventListener('click', (event) => {
+    evt.preventDefault();
+    modal.style.display = "none";
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    modal.style.display = "none";
+  }
+});
