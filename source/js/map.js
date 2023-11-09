35027861328beeba1b33b3af24381e8ac3d3883b
Georgij Tsabiev <gtsabiev@inbox.ru>
2023-11-09 17:15:14 +0300

    Поправляет размеры и сетку у карты

--- source/js/map.js
+++ source/js/map.js
@@ -2,14 +2,14 @@
 
 let screenWidth = window.screen.width;
 let mapWrapper = document.querySelector('.page__map');
-let mapImage = document.querySelector('.contacts__map-image');
+let mapImage = document.querySelector('.contacts__map-image-wrapper');
 let center = [34.866849964021355,-111.76106949186402];
 let pinSize = [27, 27];
 let pinOffset = [-10, -20];
 let zoom = 7;
 
 mapWrapper.classList.remove('map--no-js');
-mapImage.classList.remove('contacts__map-image--no-js');
+mapImage.classList.remove('contacts__map-image-wrapper--no-js');
 
 function init() {
   let map = new ymaps.Map('map', {
